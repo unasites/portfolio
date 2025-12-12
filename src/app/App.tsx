a6100f5acd502c1a71f941e10334be3d5543a563
LucaMimmo <171295375+LucaMimmo05@users.noreply.github.com>
2025-12-12 21:30:58 +0100

    style: update App component layout with Tailwind CSS classes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,9 @@
 const App = () => {
-  return <div>Test</div>;
+  return (
+    <div className="min-h-screen ">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">test</div>
+    </div>
+  );
 };
 
 export default App;
