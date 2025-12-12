485de128fc29c0fa6b451b892582a47c4136b752
LucaMimmo <171295375+LucaMimmo05@users.noreply.github.com>
2025-12-12 19:07:03 +0100

    style: add color variables and update Tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,25 @@
 import type { Config } from "tailwindcss";
-
 export default {
   content: ["./index.html", "./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg: "hsl(var(--color-bg))",
+        "bg-muted": "hsl(var(--color-bg-muted))",
+        text: "hsl(var(--color-text))",
+        "text-muted": "hsl(var(--color-text-muted))",
+        gradient: {
+          start: "hsl(var(--color-gradient-start))",
+          end: "hsl(var(--color-gradient-end))",
+        },
+      },
+      backgroundImage: {
+        "primary-gradient":
+          "linear-gradient(90deg, hsl(var(--color-gradient-start)), hsl(var(--color-gradient-end)))",
+        "text-gradient":
+          "linear-gradient(90deg, hsl(var(--color-text-gradient-start)), hsl(var(--color-text-gradient-middle)), hsl(var(--color-text-gradient-end)))",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
