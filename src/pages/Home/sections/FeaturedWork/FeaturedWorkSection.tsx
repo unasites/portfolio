c55e81c6141f23c86f057665ed3d5ca7bf3fc464
browny26 <115621041+browny26@users.noreply.github.com>
2025-12-14 13:55:12 +0100

    feat: refactor FeaturedWorkSection layout for improved structure and styling

--- src/pages/Home/sections/FeaturedWork/FeaturedWorkSection.tsx
+++ src/pages/Home/sections/FeaturedWork/FeaturedWorkSection.tsx
@@ -1,18 +1,23 @@
-import PageContainer from "@/layout/PageContainer";
 import VisualShowcase from "./VisualShowcase";
 
 const FeaturedWorkSection = () => {
   return (
     <section className="relative overflow-x-hidden overflow-y-visible w-full py-20 lg:py-38">
-      <PageContainer className="flex w-full items-center">
-        <h2 className="max-w-lg text-6xl font-normal bg-text-gradient bg-clip-text text-transparent">
-          We turn ideas into websites that push your business ahead.
-        </h2>
-      </PageContainer>
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+          <div className="flex items-center">
+            <div className="lg:max-w-lg">
+              <h2 className="max-w-lg text-6xl font-normal bg-text-gradient bg-clip-text text-transparent">
+                We turn ideas into websites that push your business ahead.
+              </h2>
+            </div>
+          </div>
 
-      {/* Right visual showcase */}
-      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center translate-x-1/4">
-        <VisualShowcase />
+          {/* Right visual showcase */}
+          <div className="pointer-events-none">
+            <VisualShowcase />
+          </div>
+        </div>
       </div>
     </section>
   );
