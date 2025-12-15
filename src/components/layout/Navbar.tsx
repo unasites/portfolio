0de8592729e952498a8b8c7fd59cd59600627ad9
LucaMimmo <171295375+LucaMimmo05@users.noreply.github.com>
2025-12-15 09:11:16 +0100

    feat: update Navbar z-index for improved layering

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -3,7 +3,7 @@ import NavLinks from "../navigation/NavLinks";
 
 const Navbar = () => {
   return (
-    <nav className="w-full relative z-20">
+    <nav className="w-full relative z-50">
       <div className="px-3 sm:px-4 lg:px-6 h-16 flex items-center justify-between">
         <Logo />
         <NavLinks />
