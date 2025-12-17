53f58ed182f0963200cdcca821581821e5f7872d
LucaMimmo <171295375+LucaMimmo05@users.noreply.github.com>
2025-12-17 15:38:49 +0100

    feat: enhance MeetOurTeam layout with improved spacing and structure; update Developer component styling

--- src/pages/Home/sections/MeetOurTeam/Developer.tsx
+++ src/pages/Home/sections/MeetOurTeam/Developer.tsx
@@ -11,7 +11,7 @@ const Developer = ({ data }: DeveloperProps) => {
   return (
     <div
       className="rounded-full bg border border-[#1A1A1A] shadow-[0_0_100px_16px_rgba(26,26,26,0.5)]
-            will-change-transform h-120 w-60 flex flex-col items-center justify-end gap-0.5 text-center p-10"
+            will-change-transform h-120 w-60 flex flex-col items-center justify-end gap-0.5 text-center p-10 nth-[2]:mt-20"
     >
       <p className="text-base font-bold ">{data.name}</p>
       <p className="text-text-muted text-base leading-tight">{data.role}</p>
--- src/pages/Home/sections/MeetOurTeam/MeetOurTeam.tsx
+++ src/pages/Home/sections/MeetOurTeam/MeetOurTeam.tsx
@@ -8,13 +8,13 @@ const MeetOurTeam = () => {
     { img: "", name: "Luisa Cerin", role: "Web Developer" },
   ];
   return (
-    <section className="w-full min-h-screen flex flex-col py-20">
+    <section className="w-full min-h-screen flex flex-col xl:gap-0 md:gap-2 gap-6 py-20">
         <h2 className="text-4xl sm:text-5xl font-normal text-center ">
           Meet our <span className="bg-primary-gradient font-bold bg-clip-text text-transparent">team</span>
         </h2>
 
         <PageContainer className="flex-1 flex items-center justify-center w-full">
-          <div className="flex flex-wrap w-full justify-between  gap-12">
+          <div className="flex flex-wrap w-full xl:gap-20 justify-center  gap-12">
             {developers.map((dev) => (
               <Developer key={dev.name} data={dev} />
             ))}
