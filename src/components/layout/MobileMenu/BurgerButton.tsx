0a260340a0f5f08592bc37663e0b33fb88d88f6d
LucaMimmo <171295375+LucaMimmo05@users.noreply.github.com>
2025-12-17 11:55:40 +0100

    feat: refactor BurgerButton to use framer-motion for animations; enhance button styling and structure

--- src/components/layout/MobileMenu/BurgerButton.tsx
+++ src/components/layout/MobileMenu/BurgerButton.tsx
@@ -1,3 +1,5 @@
+import { motion, MotionConfig } from "framer-motion";
+
 type Props = {
   isOpen: boolean;
   onToggle: () => void;
@@ -5,41 +7,57 @@ type Props = {
 
 const BurgerButton = ({ isOpen, onToggle }: Props) => {
   return (
-    <button
-      onClick={onToggle}
-      className="flex flex-col w-fit h-fit "
-      aria-label="Toggle menu"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="40"
-        height="40"
-        viewBox="0 0 40 40"
-        fill="none"
+    <MotionConfig transition={{ duration: 0.4, ease: "easeInOut" }}>
+      <motion.button
+        initial={false}
+        animate={isOpen ? "open" : "closed"}
+        onClick={onToggle}
+        className="relative h-10 w-10 flex flex-col justify-center items-center gap-2.5"
+        aria-label="Toggle menu"
       >
-        <path
-          d="M11.6666 8.33337H28.3333"
-          stroke="white"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+        <motion.span
+          className="h-[1.5px] bg-white rounded-full w-4.25"
+          variants={{
+            open: {
+              y: 11.5,
+              rotate: 45,
+              width: 28,
+            },
+            closed: {
+              y: 0,
+              rotate: 0,
+              width: 17,
+            },
+          }}
         />
-        <path
-          d="M6.66663 20H33.3333"
-          stroke="white"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+        <motion.span
+          className="h-[1.5px] bg-white rounded-full w-6.75"
+          variants={{
+            open: {
+              opacity: 0,
+            },
+            closed: {
+              opacity: 1,
+            },
+          }}
         />
-        <path
-          d="M11.6666 31.6666H28.3333"
-          stroke="white"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+        <motion.span
+          className="h-[1.5px] bg-white rounded-full w-4.25"
+          variants={{
+            open: {
+              y: -11.5,
+              rotate: -45,
+              width: 28,
+            },
+            closed: {
+              y: 0,
+              rotate: 0,
+              width: 17,
+            },
+          }}
         />
-      </svg>
-    </button>
+      </motion.button>
+    </MotionConfig>
   );
 };
 
